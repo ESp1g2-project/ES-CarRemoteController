35c1dcb185a87a6a0412be8094d72e646be52194
Diogo <diogo.alex5@hotmail.com>
2019-05-18 10:15:58 -0700

    added target and resources empty directories

--- LegoCarRemoteController/src/main/java/com/ua/LegoCarRemoteController/resource/KafkaProducerCarControls.java
+++ LegoCarRemoteController/src/main/java/com/ua/LegoCarRemoteController/resource/KafkaProducerCarControls.java
@@ -17,7 +17,7 @@ public class KafkaProducerCarControls {
 
 	@Autowired
 	private KafkaTemplate<String, Command> kafkaTemplate;
-	private static final String TOPIC = "ESp1g2Car";
+	private static final String TOPIC = "p1g2car";
 	
 	/*@GetMapping("/publish/{message}")
 	public String post(@PathVariable("message") final String message)
